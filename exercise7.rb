80e4c8da6b647ed0c702bb8be3c25390c2b5a97a
Matthew Alexander Smith <matthewalexander108@gmail.com>
2016-01-15 17:55:35 -0500

    Exercise 7 - Question 7

--- exercise7.rb
+++ exercise7.rb
@@ -1,9 +1,33 @@
+#Question 1
 students = {
   :cohort1 => 34,
   :cohort2 => 42,
   :cohort3 => 22,
+  :cohort4 => 43
 }
 
+puts "Print Hash"
+students.each do |k, v|
+  puts "#{k}: #{v}"
+end
+puts "Print the sum of all values in students hash"
+sum = 0
+students.each {|key, value| sum += value}
+puts sum
+puts""
+
+puts "Print hash multiplied by 5%"
+students.each do |k, v|
+  puts "#{k}: #{v*1.05}"
+end
+puts "Print the sum of all values in students hash"
+sum = 0.0
+students.each {|key, value| sum += value}
+puts sum
+puts""
+
+puts "Delete second entry and print hash"
+students.delete(:cohort2)
 students.each do |k, v|
   puts "#{k}: #{v}"
 end
