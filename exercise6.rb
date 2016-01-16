200cefe0dd31f838dbb2e3fd94528e57d44610aa
Matthew Alexander Smith <matthewalexander108@gmail.com>
2016-01-15 23:34:15 -0500

    Exercise 6 - Tidy up

--- exercise6.rb
+++ exercise6.rb
@@ -3,9 +3,9 @@ class GroceryList
 #Initialize is called when the new method is used
 #to create a new instance of the class.
 
-def initialize
-  @grocery_list = ["carrots", "toilet paper", "apples", "salmon"]
-end
+  def initialize
+    @grocery_list = ["carrots", "toilet paper", "apples", "salmon"]
+  end
 
   def add (add_item)
     if add_item == "y"
@@ -22,21 +22,19 @@ end
     end
   end
 
+  puts "Returns the first draft of your grocery list"
   def print_list
-    puts ""
-
     @grocery_list.each do |item|
       puts "*#{item}"
     end
     puts ""
 
-    puts "Alphabetically sorted grocery list:"
+    puts "Returns alphabetically sorted grocery list"
     @grocery_list = @grocery_list.sort
     @grocery_list.each do |item|
       puts "*#{item}"
     end
     puts ""
-
 
     puts "There wasn't any salmon!"
     puts "Here is the list without salmon"
@@ -44,28 +42,26 @@ end
     @grocery_list.each do |item|
       puts "*#{item}"
     end
-
-    if @grocery_list.include? 'bananas'
-      puts "You don't need to pick up bananas"
-      puts ""
-    else
-      puts "You need to pick up bananas!"
-      puts ""
-    end
-    puts "The second item in the list is #{@grocery_list[1]}"
     puts ""
 
-end
+    if @grocery_list.include? 'bananas'
+      puts "You don't need to pick up bananas\r\n"
+    else
+      puts "You need to pick up bananas!\r\n"
+    end
+
+    puts "The second item in the list is #{@grocery_list[1]}!!\n\r"
+  end
 
   def action_loop
     while true
       print_list
-      puts "Add Item? (y/n)"
+      puts "Add another item to the list? (y/n)"
       add_item = gets.chomp.to_s
       add(add_item)
     end
   end
 end
 
-# groceries = GroceryList.new
-# groceries.action_loop
+groceries = GroceryList.new
+groceries.action_loop
